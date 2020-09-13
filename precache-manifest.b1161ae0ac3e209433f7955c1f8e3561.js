5a244da7a7753acae82acaa805b2ff4fe2243456
Patrick K <patrickkendall@live.com>
2020-09-13 13:07:13 -0400

    Updates

---
+++ precache-manifest.b1161ae0ac3e209433f7955c1f8e3561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30593c4a31220712b734678fd77b31a",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "b81f0e4d8c6b6d248a95",
+    "url": "/chat-app/static/css/main.258f8489.chunk.css"
+  },
+  {
+    "revision": "f226d9d8e13aeae8b71e",
+    "url": "/chat-app/static/js/2.0f84ecfa.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/chat-app/static/js/2.0f84ecfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81f0e4d8c6b6d248a95",
+    "url": "/chat-app/static/js/main.05d2ac39.chunk.js"
+  },
+  {
+    "revision": "a46df673de3fe3887809",
+    "url": "/chat-app/static/js/runtime-main.0c4eda3f.js"
+  }
+]);
